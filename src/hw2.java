2ddebdf4a1bafc30b5247e36648fa8adeb565607
penis <niakids95@gmail.com>
2026-06-26 18:36:40 +0600

    do homework2

---
+++ src/hw2.java
@@ -0,0 +1,25 @@
+public class hw2 {
+    public static void main(String[] args) {
+
+
+        // 1.
+
+        int a0 = 5;
+        int a1 = 11;
+        double b0 = 5.5;
+        double b1 = 1.3;
+        long c0 = 20l;
+        double res = 0;
+        res = a1 / b0 + b1 % a0 - c0;
+
+        System.out.println(res);
+
+
+        // 2.
+
+        // 5 - 3 + 4 + 4 + 5 = 15
+        // 9 - 9 + 11 - 10 = 1
+
+
+    }
+}
