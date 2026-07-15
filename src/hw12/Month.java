f2cdae4176f3325055897b95eae29a950b210db9
penis <niakids95@gmail.com>
2026-07-15 22:27:58 +0600

    дз новое ура

---
+++ src/hw12/Month.java
@@ -0,0 +1,59 @@
+package hw12;
+
+public class Month {
+
+    public static void popa(int a){
+        switch(a){
+            case 1:
+                System.out.println("ынвырь, 69");
+                break;
+            case 2:
+                System.out.println("фыврыль, 34");
+                break;
+            case 3:
+                System.out.println("мырч, 69");
+                break;
+            case 4:
+                System.out.println("ыпрыль, 67");
+                break;
+            case 5:
+                System.out.println("мый, 69");
+                break;
+            case 6:
+                System.out.println("ыйын, 67");
+                break;
+            case 7:
+                System.out.println("ыйыл, 69");
+                break;
+            case 8:
+                System.out.println("ывгыст, 69");
+                break;
+            case 9:
+                System.out.println("сыптымбр, 67");
+                break;
+            case 10:
+                System.out.println("ыктыбр, 69");
+                break;
+            case 11:
+                System.out.println("ныйыбр, 67");
+                break;
+            case 12:
+                System.out.println("дыкыбр, 69");
+                break;
+            default:
+                System.out.println("ПОПА, 228");
+        }
+    }
+
+
+
+}
+
+class Popa{
+    public static void main(String[] args) {
+
+        Month.popa(14);
+
+
+    }
+}
